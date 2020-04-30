7923fd6d5f38a66a421e49aab9bc417f9db7e50e
adldotori <adldotori@gmail.com>
2020-04-30 17:35:31 +0000

    Change readme

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 for CIFAR-10
 
-<https://arxiv.org/pdf/1411.1784.pdf>
+<https://arxiv.org/pdf/1512.03385.pdf>
--- dataloader.py
+++ dataloader.py
@@ -16,15 +16,13 @@ class CIFAR10Dataset(data.Dataset):
                             transforms.RandomCrop(32, padding=4),
                             transforms.RandomHorizontalFlip(),
                             transforms.ToTensor(),
-                            transforms.Normalize(mean=(0.5,), std=(0.5,))
+                            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
                         ]))
         else:
             self.dataset = datasets.CIFAR10('./data', train=False, download=True,
                         transform=transforms.Compose([
-                            transforms.RandomCrop(32, padding=4),
-                            transforms.RandomHorizontalFlip(),
                             transforms.ToTensor(),
-                            transforms.Normalize(mean=(0.5,), std=(0.5,))
+                            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
                         ])) 
 
     def name(self):
